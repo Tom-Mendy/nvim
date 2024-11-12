ba6255297a53c4bf6df57df9685164a152a3467e
Tom Mendy <tom.mendy@epitech.eu>
2024-11-12 11:28:12 +0100

    [DEL] useless plugin

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -1,19 +1,6 @@
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
-end
-vim.opt.rtp:prepend(lazypath)
-
 
 require('lazy').setup({
 
@@ -22,7 +9,6 @@ require('lazy').setup({
   'tpope/vim-surround',
   'tpope/vim-obsession',
   'tpope/vim-rhubarb',
-
   -- detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
@@ -59,7 +45,7 @@ require('lazy').setup({
   },
 
   -- markdown preview
-  'iamcco/markdown-preview.nvim',
+  -- 'iamcco/markdown-preview.nvim',
 
   -- DAP
   {
@@ -245,10 +231,6 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
   {
-    "ecthelionvi/NeoView.nvim",
-    opts = {}
-  },
-  {
     "nvim-tree/nvim-tree.lua",
     version = "*",
     lazy = false,
@@ -260,26 +242,8 @@ require('lazy').setup({
     end,
   },
   {
-    'stevearc/conform.nvim',
-    opts = {},
-  },
-  {
     'akinsho/bufferline.nvim',
     version = "*",
     dependencies = 'nvim-tree/nvim-web-devicons'
-  },
-  {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    ---@type Flash.Config
-    opts = {},
-    -- stylua: ignore
-    keys = {
-      { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-      { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-      { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-      { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
-    },
   },
 }, {})
